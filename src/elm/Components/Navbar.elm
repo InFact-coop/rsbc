daf209518e76c206c16d8fb854c3981ad8a93936
astroash <astroaisha@gmail.com>
2017-11-28 15:55:03 +0000

    #15 Add styles and logo to nav

--- src/elm/Components/Navbar.elm
+++ src/elm/Components/Navbar.elm
@@ -7,13 +7,19 @@ import Types exposing (..)
 
 navbar : Model -> Html Msg
 navbar model =
-    ul [ class "dib ma0 bg-green w-100 pa2" ]
-        navbarContent
+    let
+        navImg =
+            [ img [ src "./assets/rsbc_logo.png", class "h3" ] []
+            ]
+    in
+        ul [ class "dib ma0 navbar w-100 pa2 flex justify-between" ] <|
+            navImg
+                ++ [ div [] navbarContent ]
 
 
 navbarLink : ( String, String ) -> Html Msg
 navbarLink ( linkStr, name ) =
-    li [ class "list dib ma3" ] [ a [ class "link dim white b", href ("/#" ++ linkStr) ] [ text name ] ]
+    li [ class "list dib ma3 v-top" ] [ a [ class "link dim white b", href ("/#" ++ linkStr) ] [ text name ] ]
 
 
 navbarContent : List (Html Msg)
