08e8e6b87ed7a4e21264061d8569ea9fddc2225b
Rebecacalvoquintero <rebecacalvoquintero@hotmail.es>
2017-11-28 18:05:53 +0000

    #3 map api working

---
+++ public/map.js
@@ -0,0 +1,49 @@
+function initMap() {
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 13,
+    center: {
+      lat:   51.516584,
+      lng: -0.103096
+    }
+  });
+
+
+  // Create an array of alphabetical characters used to label the markers.
+  var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+  // Add some markers to the map.
+  // Note: The code uses the JavaScript Array.prototype.map() method to
+  // create an array of markers based on a given "locations" array.
+  // The map() method here has nothing to do with the Google Maps API.
+  var markers = locations.map(function(location, i) {
+    return new google.maps.Marker({
+      position: location,
+      label: labels[i % labels.length]
+    });
+  });
+
+  // Add a marker clusterer to manage the markers.
+  var markerCluster = new MarkerClusterer(map, markers, {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+}
+var locations = [
+   {
+    lat: 51.541532,
+    lng: -0.061594
+  }, {
+    lat: 51.535994,
+    lng: -0.048638
+  }, {
+    lat: 51.542063,
+    lng: -0.069984
+  }, {
+    lat: 51.546192,
+    lng: -0.074045
+  }, {
+    lat: 51.539394,
+    lng: -0.083675
+  }, {
+    lat: 51.523713,
+    lng: -0.082138
+  }
+]
--- src/elm/Routes/MapPage.elm
+++ src/elm/Routes/MapPage.elm
@@ -8,6 +8,5 @@ import Types exposing (..)
 mapPage : Model -> Html Msg
 mapPage model =
     div [ class "w-60-ns center" ]
-        [ h1 [ class "tc f1" ] [ text "map" ]
-        , p [ class "f3 w60 mh1 tc" ] [ text "map map" ]
+        [ div [ id "map" ] []
         ]
