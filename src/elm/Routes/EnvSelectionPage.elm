63ba6ce60772f80ae2c9f698da5313a72ef79ce3
Rebecacalvoquintero <rebecacalvoquintero@hotmail.es>
2017-12-01 17:04:51 +0000

    #22 finished envPage

--- src/elm/Routes/EnvSelectionPage.elm
+++ src/elm/Routes/EnvSelectionPage.elm
@@ -8,8 +8,18 @@ import Types exposing (..)
 
 envSelectionPage : Model -> Html Msg
 envSelectionPage model =
-    div [ class "pt3 center w-60-ns tc" ]
-        [ h1 [ class "tc f1" ] [ text "" ]
-        , imgButton "grow black wide-spacing pa2 ma1 dib" "Adapting the Environment" "" "subject" "./assets/calculator.png"
-        , imgButton "grow black wide-spacing pa2 ma1 dib" "Tech Aids" "" "subject" "./assets/calculator.png"
+    div [ class "mt4 pt3 ma3 center w-60-ns tc " ]
+        [ div [ class "mt4" ] []
+        , div []
+            [ div [ class "flex justify-center space-around" ]
+                [ imgButton " ma2 ba b--black grow black wide-spacing pa2 ma1 w-30 vh-33 bg-white no-underline tc" "Adapting the Environment" "" "subject" "./assets/environment.png"
+                , imgButton " ma2 ba b--black grow black wide-spacing pa2 ma1 w-30 vh-33 bg-white no-underline tc" "Tech Aids" "" "subject" "./assets/tech.png"
+                ]
+            ]
+        , div []
+            [ div [ class "flex justify-center space-around" ]
+                [ imgButton " ma2 ba b--black grow black wide-spacing pa2 ma1 w-30 vh-33 bg-white no-underline tc" "Option C" "" "subject" "./assets/optionc.png"
+                , imgButton " ma2 ba b--black grow black wide-spacing pa2 ma1 w-30 vh-33 bg-white no-underline tc" "Option D" "" "subject" "./assets/optionD.png"
+                ]
+            ]
         ]
