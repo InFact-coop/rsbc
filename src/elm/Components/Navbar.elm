742228c9f8746adf9244da6a7cb2b4e0d8e11604
Matt Davis <matthewpeterdavis@gmail.com>
2017-12-01 17:02:29 +0000

    tidy up css

--- src/elm/Components/Navbar.elm
+++ src/elm/Components/Navbar.elm
@@ -13,16 +13,16 @@ navbar model =
             [ img [ src "./assets/rsbc_logo.png", class "h3" ] []
             ]
     in
-        ul [ class "black dib ma0 navbar w-100 pa2 flex justify-between" ] <|
-            navImg
-                ++ [ div [] <| navbarContent model ]
+    ul [ class "black dib ma0 navbar w-100 pa2 flex justify-between" ] <|
+        navImg
+            ++ [ div [] <| navbarContent model ]
 
 
 navbarLink : ( String, String, Bool ) -> Html Msg
 navbarLink ( linkStr, name, onPage ) =
     li [ class "list dib ma3 v-top" ]
         [ a
-            [ class "b--rsbc-red no-underline dim black wide-spacing pb2"
+            [ class "b--rsbc-red no-underline dim black wide-spacing pb3"
             , classList [ ( "bb", onPage ) ]
             , href ("/" ++ linkStr)
             ]
@@ -33,11 +33,11 @@ navbarLink ( linkStr, name, onPage ) =
 navbarContent : Model -> List (Html Msg)
 navbarContent model =
     List.map navbarLink
-        [ ( "#home", "home", isActivePage model "#home" )
-        , ( "#subjectselection", "curriculum", isActivePage model "#subjectselection" )
-        , ( "#envselection", "environment", isActivePage model "#envselection" )
-        , ( "#about", "us & you", isActivePage model "#about" )
-        , ( "#map", "near me", isActivePage model "#map" )
+        [ ( "#home", "HOME", isActivePage model "#home" )
+        , ( "#subjectselection", "CURRICULUM", isActivePage model "#subjectselection" )
+        , ( "#envselection", "ENVIRONMENT", isActivePage model "#envselection" )
+        , ( "#about", "US & YOU", isActivePage model "#about" )
+        , ( "#map", "NEAR ME", isActivePage model "#map" )
         ]
 
 
--- src/elm/Routes/Home.elm
+++ src/elm/Routes/Home.elm
@@ -10,17 +10,17 @@ import Types exposing (..)
 home : Model -> Html Msg
 home model =
     div [ class "w-75-ns center" ]
-        [ h1 [ class "tc pa0 ma0 f1" ]
+        [ h1 [ class "tc pa0 ma0 f2 pt4" ]
             [ text "RSBC SENDCO Resources" ]
-        , p [ class "w-75 tc center f3 ma4 mb5 pa1" ] [ text "A platform for SENDCO's across the country to find resources and share them with visually impaired students and their families" ]
+        , p [ class "w-75 tc center f4 ma4 mb5 pa1" ] [ text "A platform for SENDCO's across the country to find resources and share them with visually impaired students and their families" ]
         , div
-            [ class "inline-flex justify-between w-100 mv3" ]
-            [ buttonMaker buttonClass "Curriculum" "what's in the curriculum" "subjectselection"
-            , buttonMaker buttonClass "Environment" "what's in the environment" "envselection"
-            , buttonMaker buttonClass "Us & You" "what about us" "about"
+            [ class "inline-flex justify-between w-100 mv3 pa1" ]
+            [ buttonMaker buttonClass "Curriculum" "Find resources to help with particular curriculum subjects" "subjectselection"
+            , buttonMaker buttonClass "Environment" "Resources to help with what you need to do on the arrival of a new VI student" "envselection"
+            , buttonMaker buttonClass "Us & You" "What RSBC can do for you and what you can do for us" "about"
             ]
         ]
 
 
 buttonClass =
-    "ba b--black grow black wide-spacing pa2 ma1 w-30 vh-33 bg-white no-underline tc"
+    "ba b--black grow black pa2 ma1 w-30 vh-33 bg-white no-underline tc"
