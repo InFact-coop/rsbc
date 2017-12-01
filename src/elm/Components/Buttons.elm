6ab469aedc1d0ebb944e542c0362b851d8355e97
Rebecacalvoquintero <rebecacalvoquintero@hotmail.es>
2017-12-01 13:07:12 +0000

    styled subjects and map

--- src/elm/Components/Buttons.elm
+++ src/elm/Components/Buttons.elm
@@ -17,3 +17,17 @@ buttonMaker buttonClass heading body linkStr =
             , p [] [ text body ]
             ]
         ]
+
+
+imgButton : String -> String -> String -> String -> String -> Html Msg
+imgButton buttonClass heading body linkStr imgSrc =
+    a
+        [ class "no-underline w-25 dib pa1"
+        , href ("/#" ++ linkStr)
+        ]
+        [ div [ class ("tc justify-center no-repeat flex items-end ma3 bg-center contain" ++ buttonClass), style [ ( "background-image", "url(" ++ imgSrc ++ ")" ), ( "height", "10rem" ) ] ] []
+        , div []
+            [ h1 [ class "tc" ] [ text heading ]
+            , p [] [ text body ]
+            ]
+        ]
--- src/elm/Routes/SubjectSelectionPage.elm
+++ src/elm/Routes/SubjectSelectionPage.elm
@@ -11,11 +11,21 @@ subjectSelectionPage model =
     div [ class "w-60-ns center" ]
         [ h1 [ class "tc f1" ]
             [ text "Subjects" ]
-        , buttonMaker "mathImage ba b--rsbc-red grow black wide-spacing pa2 ma1 dib" "Maths" "" "subject"
-        , buttonMaker "ba b--rsbc-red grow black wide-spacing pa2 ma1 dib" "Maths" "" "subject"
-        , buttonMaker "ba b--rsbc-red grow black wide-spacing pa2 ma1 dib" "Maths" "" "subject"
-        , buttonMaker "ba b--rsbc-red grow black wide-spacing pa2 ma1 dib" "Maths" "" "subject"
-        , buttonMaker "ba b--rsbc-red grow black wide-spacing pa2 ma1 dib" "Maths" "" "subject"
-        , buttonMaker "ba b--rsbc-red grow black wide-spacing pa2 ma1 dib" "Maths" "" "subject"
-        , buttonMaker "ba b--rsbc-red grow black wide-spacing pa2 ma1 dib" "Maths" "" "subject"
+        , div [ class "inline-flex flex-wrap justify-around" ]
+            [ imgButton " grow black wide-spacing dib"
+                "Maths"
+                ""
+                "subject"
+                "./assets/calculator.png"
+            , imgButton "grow black wide-spacing dib"
+                "Science"
+                ""
+                "subject"
+                "./assets/scienceIcon.png"
+            , imgButton " grow black wide-spacing dib" "Arts" "" "subject" "./assets/paintIcon.png"
+            , imgButton " grow black wide-spacing dib" "Reading" "" "subject" "./assets/readingIcon.png"
+            , imgButton " grow black wide-spacing dib" "Sport" "" "subject" "./assets/sportsIcon.png"
+            , imgButton " grow black wide-spacing dib" "Homework" "" "subject" "./assets/homeworkIcon.png"
+            , imgButton " grow black wide-spacing dib" "All" "" "subject" "./assets/everythingIcon.png"
+            ]
         ]
